98d879d8c47488f5c03d26e84c3a88335fc292fa
Arjun <arjunsuthar2112@gmail.com>
2023-12-22 13:49:41 +0530

    first commit

---
+++ src/card.js
@@ -0,0 +1,33 @@
+import React, { useEffect ,useState } from 'react'
+
+import { Link } from 'react-router-dom'
+const Card = ({movie}) => {
+    const [isloading , setIsLoading] = useState(true);
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false);
+        }, 1500)
+}, [])
+  return <>
+  {
+     
+     <Link  to={`/movie/${movie.id}`}>
+        <div className=' relative hover:scale-110' ><img  className='rounded-xl w-60 m-5  ' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" />
+         
+       <div className='text-white absolute w-60 bottom-0 opacity-0 hover:opacity-100  h-full  p-10'>
+        <div className='absolute bottom-10 hover:scale-110 h-3/5'> 
+        <div  className=' title text-sm font-bold '>{movie.original_title}</div>
+        <div className='text-sm '>{movie.release_date}</div>
+        <div className=' text-sm '>{movie.vote_average}</div>
+        <div  className='text-white  text-left text-sm '>{(movie.overview.slice(0,118))+"...."}</div>
+        </div> 
+        </div>
+        </div>
+     </Link>
+  }
+  </>
+    
+  
+}
+
+export default Card
